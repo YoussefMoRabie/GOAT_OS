067b368bb4c606bdde7499348ec72ea068144c65
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-04-12 01:57:09 +0200

    scratching the SRTN algo.

--- scheduler.c
+++ scheduler.c
@@ -1,36 +1,71 @@
 #include "headers.h"
 
-typedef enum pState{
-    Ready,
-    Running,
-}pState;
+// typedef enum pState{
+//     Ready,
+//     Running,
+// }pState;
 
-typedef struct PCB{
-    int pid;
-    pState p_state;
-    int remaining_time;
-    int running_time;
-    int waiting_time;
+// typedef struct PCB{
+//     int pid;
+//     pState p_state;
+//     int remaining_time;
+//     int running_time;
+//     int waiting_time;
 
-} PCB;
+// } PCB;
 
-typedef struct process_table{
-    //to be continued
-} process_table;
+// typedef struct process_table{
+//     //to be continued
+// } process_table;
 
+void Termination_SIG(int signnum);
+
+Process *curProc;
+Queue *RQ;
+
+void SRTN(){
+    if(curProc->remainingTime > front(RQ)->remainingTime){
+        //context switch
+        kill(curProc->id,SIGSTOP);
+        InsertAccordingTo(RQ,curProc,RemainingTime);
+        curProc=dequeue(RQ);
+    }
+    if (curProc->remainingTime==curProc->runningTime)
+    {
+        //create new process and run it
+        int pid=fork();
+        if (pid ==0){
+            curProc->id=getpid();
+            execlp('./proc','./proc',&curProc->remainingTime,NULL);
+        }
+    }
+    else {
+        kill(curProc->id,SIGCONT);
+
+    }
+}
+void terminateCur(){
+    curProc=dequeue(RQ);
+}
 int main(int argc, char * argv[])
 {
     initClk();
-    Queue *RQ_addr= attach_RQ();
+    RQ= attach_RQ();
+    signal(SIGUSR1,Termination_SIG);
     //TODO implement the scheduler :)
     //upon termination release the clock resources.
     
     //remove this later
     printf("scheduler is working with algorithm id: %s", argv[1]);
+    curProc=dequeue(RQ);
     while(1)
     {
         sleep(100);
     }
     
     destroyClk(true);
+}
+
+void Termination_SIG(int signnum){
+    terminateCur();
 }
